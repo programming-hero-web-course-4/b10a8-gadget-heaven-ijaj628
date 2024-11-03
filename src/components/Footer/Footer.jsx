6e9a145d5f4d5c0aa0b4407d78d3f668fb237256
ijaj628 <rijajur019@gmail.com>
2024-11-03 22:18:32 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,25 +3,35 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <footer className="footer bg-neutral text-neutral-content p-10">
+            <div className='text-center'>
+                <h2 className='text-3xl font-bold mb-3'>Gadget Heaven</h2>
+                <p className='border-b-2 mx-36 pb-5'>Best Offer Shopping Platform</p>
+            </div>
+            <footer className="grid grid-cols-1 lg:grid-cols-3 p-10 text-center gap-5 lg:gap-0">
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h6 className="text-xl font-bold mb-3">Services</h6>
+                    <a className="link link-hover">Product Support</a>
+                    <br />
+                    <a className="link link-hover">Order Tracking</a>
+                    <br />
+                    <a className="link link-hover">Shipping & Delivery</a>
+                    <br />
+                    <a className="link link-hover">Returns</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Company</h6>
+                    <h6 className="text-xl font-bold mb-3">Company</h6>
                     <a className="link link-hover">About us</a>
+                    <br />
+                    <a className="link link-hover">Careers</a>
+                    <br />
                     <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
+                    <h6 className="text-xl font-bold mb-3">Legal</h6>
                     <a className="link link-hover">Terms of use</a>
+                    <br />
                     <a className="link link-hover">Privacy policy</a>
+                    <br />
                     <a className="link link-hover">Cookie policy</a>
                 </nav>
             </footer>
